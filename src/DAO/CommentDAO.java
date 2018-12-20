f6640634e79c442d9304507226a2f76ffcfed473
xuzhenyu <37819120+knight555@users.noreply.github.com>
2018-12-20 23:10:56 +0800

    Add files via upload

---
+++ src/DAO/CommentDAO.java
@@ -0,0 +1,13 @@
+package DAO;
+import java.util.List;
+import Bean.*;
+
+public interface CommentDAO {
+
+	public void addComment(Comment m) throws DAOException;
+	public void updateComment(Comment m) throws DAOException;
+	public Comment getComment(int commentid) throws DAOException;
+	public void deleteComment(int commentid) throws DAOException;
+	public List<Comment> Search(String name) throws DAOException;
+	
+}
---
+++ src/DAO/MovieDAO.java
@@ -0,0 +1,13 @@
+package DAO;
+import java.util.List;
+import Bean.*;
+
+public interface MovieDAO {
+	
+	public void addMovie(Movie m) throws DAOException;
+	public void updateMovie(Movie m) throws DAOException;
+	public Movie getMovie(int movieid) throws DAOException;
+	public void deleteMovie(int movieid) throws DAOException;
+	public List<Movie> Search(String name) throws DAOException;
+	
+}
