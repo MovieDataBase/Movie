132850d82b68b1595308b721ff3b6566c29349f1
YashajinAi <40882098+YashajinAi@users.noreply.github.com>
2018-12-22 14:08:58 +0800

    Update create.sql

--- code/create.sql
+++ code/create.sql
@@ -1,5 +1,12 @@
 if exists (select 1
             from  sysobjects
+           where  id = object_id('"browse"')
+            and   type = 'U')
+   drop table "browse"
+go
+
+if exists (select 1
+            from  sysobjects
            where  id = object_id('comment')
             and   type = 'U')
    drop table comment
@@ -21,6 +28,13 @@ go
 
 if exists (select 1
             from  sysobjects
+           where  id = object_id('"like"')
+            and   type = 'U')
+   drop table "like"
+go
+
+if exists (select 1
+            from  sysobjects
            where  id = object_id('movie')
             and   type = 'U')
    drop table movie
@@ -34,12 +48,24 @@ if exists (select 1
 go
 
 /*==============================================================*/
+/* Table: "browse"                                              */
+/*==============================================================*/
+create table "browse" (
+   browseid             int                  not null,
+   userid               int                  not null,
+   movieid              int                  not null,
+   broustime            datetime             not null,
+   constraint PK_BROWSE primary key nonclustered (browseid)
+)
+go
+
+/*==============================================================*/
 /* Table: comment                                               */
 /*==============================================================*/
 create table comment (
-   commentid            char(20)             not null,
-   userid               char(10)             not null,
-   movieid              char(10)             not null,
+   commentid            int                  not null,
+   userid               int                  not null,
+   movieid              int                  not null,
    text                 varchar(1000)        not null,
    time                 datetime             not null,
    score                int                  not null,
@@ -51,8 +77,8 @@ go
 /* Table: direct                                                */
 /*==============================================================*/
 create table direct (
-   directorid           char(10)             not null,
-   movieid              char(10)             not null,
+   directorid           int                  not null,
+   movieid              int                  not null,
    constraint PK_DIRECT primary key nonclustered (directorid, movieid)
 )
 go
@@ -61,10 +87,10 @@ go
 /* Table: director                                              */
 /*==============================================================*/
 create table director (
-   directorid           char(10)             not null,
+   directorid           int                  not null,
    name                 varchar(100)         not null,
    sex                  varchar(10)          not null,
-   birthday             datetime             not null,
+   birthday             date                 not null,
    birthplace           varchar(100)         not null,
    profile              varchar(2000)        not null,
    constraint PK_DIRECTOR primary key nonclustered (directorid)
@@ -72,17 +98,27 @@ create table director (
 go
 
 /*==============================================================*/
+/* Table: "like"                                                */
+/*==============================================================*/
+create table "like" (
+   userid               int                  not null,
+   commentid            int                  not null,
+   constraint PK_LIKE primary key nonclustered (userid, commentid)
+)
+go
+
+/*==============================================================*/
 /* Table: movie                                                 */
 /*==============================================================*/
 create table movie (
-   movieid              char(10)             not null,
+   movieid              int                  not null,
    moviename            varchar(100)         not null,
    screenwriter         varchar(500)         not null,
    actor                varchar(1000)        not null,
    type                 varchar(50)          not null,
    country              varchar(100)         not null,
    language             varchar(50)          not null,
-   releasetime          datetime             not null,
+   releasetime          date                 not null,
    duration             int                  not null,
    introduction         varchar(2000)        not null,
    constraint PK_MOVIE primary key nonclustered (movieid)
@@ -93,7 +129,7 @@ go
 /* Table: "user"                                                */
 /*==============================================================*/
 create table "user" (
-   userid               char(10)             not null,
+   userid               int                  not null,
    username             varchar(20)          not null,
    password             varchar(20)          not null,
    email                varchar(50)          not null,
