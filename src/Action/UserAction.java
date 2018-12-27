4282a624afe8b546fb450136929ef581036c80f3
YashajinAi <40882098+YashajinAi@users.noreply.github.com>
2018-12-27 16:35:10 +0800

    Add files via upload

--- src/Action/UserAction.java
+++ src/Action/UserAction.java
@@ -1,10 +1,9 @@
 package Action;
-import DAO.*;
 import Bean.*;
+import DAO.*;
+import java.util.List;
 
 public class UserAction {
-	
-	
 	
 	public int register(User u) throws DAOException {
 		UserDAOImpl uImpl = new UserDAOImpl();
@@ -24,4 +23,56 @@ public class UserAction {
 		return true;
 	}
 	
+	// 浏览电影信息
+	public void Browse(int userid, int movieid) throws DAOException {
+		
+		MovieDAOImpl movieimpl = new MovieDAOImpl();
+		DirectorDAOImpl directorimpl = new DirectorDAOImpl();
+		BrouseDAOImpl browseimpl = new BrouseDAOImpl();
+		
+		// 获取电影信息和导演
+		Movie movie = movieimpl.getMovie(movieid);
+		List<Director> director = directorimpl.getDirector_byMovie(movieid);
+		
+		// 显示电影信息	
+		System.out.println("电影名：" + movie.getMoviename());
+		System.out.print("导演：");
+		for(int i = 0 ; i < director.size() ; i++) {
+			if(i != 0) System.out.print("、");
+		    System.out.print(director.get(i).getName());
+		}
+		System.out.println();
+		System.out.println("编剧：" + movie.getScreenwriter());
+		System.out.println("主演：" + movie.getActor());
+		System.out.println("类型：" + movie.getType());
+		System.out.println("制片国家/地区：" + movie.getCountry());
+		System.out.println("语言：" + movie.getLanguage());
+		System.out.println("上映日期：" + movie.getReleasetime());
+		System.out.println("片长：" + movie.getDuration() + "分钟");
+		System.out.println("剧情简介：" + movie.getIntroduction());
+		
+		// 添加浏览记录
+		Brouse browse = new Brouse();
+		browse.setMovieid(movieid);
+		browse.setUserid(userid);
+		
+		browseimpl.addBrouse(browse);
+		
+	}
+	
+	// 浏览历史
+	public void BrowseList(int userid) throws DAOException {
+		
+		BrouseDAOImpl browseimpl = new BrouseDAOImpl();
+		MovieDAOImpl movieimpl = new MovieDAOImpl();
+		UserDAOImpl userimpl = new UserDAOImpl();
+		
+		// 显示
+		System.out.println(userimpl.getUser(userid).getUsername() + " 的浏览记录：");
+		List<Brouse> browse = browseimpl.Search(userid);
+		for(int i = 0; i < browse.size(); i++) {
+			System.out.println("浏览电影：" + movieimpl.getMovie(browse.get(i).getMovieid()).getMoviename() + "    浏览时间：" + browse.get(i).getBrousetime());
+		}
+		
+	}
 }
