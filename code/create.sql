7e3bdc9b2246c83945c3d79e3b19a2190d14f612
SamHu <hhlchess@sina.com>
2018-12-28 00:03:26 +0800

    Update create.sql

--- code/create.sql
+++ code/create.sql
@@ -51,10 +51,10 @@ go
 /* Table: "browse"                                              */
 /*==============================================================*/
 create table "browse" (
-   browseid             int                  not null,
+   browseid             int                  identity,
    userid               int                  not null,
    movieid              int                  not null,
-   broustime            datetime             not null,
+   broustime            datetime             not null default getdate(),
    constraint PK_BROWSE primary key nonclustered (browseid)
 )
 go
@@ -63,13 +63,14 @@ go
 /* Table: comment                                               */
 /*==============================================================*/
 create table comment (
-   commentid            int                  not null,
+   commentid            int                  identity,
    userid               int                  not null,
    movieid              int                  not null,
    text                 varchar(1000)        not null,
-   time                 datetime             not null,
+   time                 datetime             not null default getdate(),
    score                int                  not null,
-   constraint PK_COMMENT primary key nonclustered (commentid)
+   constraint PK_COMMENT primary key nonclustered (commentid),
+   constraint AK_USERID_COMMENT unique (userid, movieid)
 )
 go
 
@@ -87,10 +88,10 @@ go
 /* Table: director                                              */
 /*==============================================================*/
 create table director (
-   directorid           int                  not null,
+   directorid           int                  identity,
    name                 varchar(100)         not null,
    sex                  varchar(10)          not null,
-   birthday             date                 not null,
+   birthday             datetime             not null,
    birthplace           varchar(100)         not null,
    profile              varchar(2000)        not null,
    constraint PK_DIRECTOR primary key nonclustered (directorid)
@@ -111,14 +112,14 @@ go
 /* Table: movie                                                 */
 /*==============================================================*/
 create table movie (
-   movieid              int                  not null,
+   movieid              int                  identity,
    moviename            varchar(100)         not null,
    screenwriter         varchar(500)         not null,
    actor                varchar(1000)        not null,
    type                 varchar(50)          not null,
    country              varchar(100)         not null,
    language             varchar(50)          not null,
-   releasetime          date                 not null,
+   releasetime          datetime             not null,
    duration             int                  not null,
    introduction         varchar(2000)        not null,
    constraint PK_MOVIE primary key nonclustered (movieid)
@@ -129,10 +130,11 @@ go
 /* Table: "user"                                                */
 /*==============================================================*/
 create table "user" (
-   userid               int                  not null,
+   userid               int                  identity,
    username             varchar(20)          not null,
    password             varchar(20)          not null,
    email                varchar(50)          not null,
-   constraint PK_USER primary key nonclustered (userid)
+   constraint PK_USER primary key nonclustered (userid),
+   constraint AK_USERNAME_USER unique (username)
 )
 go
